9a0ad225d6404626a5ea1ae08d0197b8f14ff89f
Trần Văn Luýt <tranvanluyt12b4@gmail.com>
2022-08-05 14:55:22 +0700

    thanhcong

---
+++ THANHCONG/home.js
@@ -0,0 +1,28 @@
+function search()
+{
+   document.getElementById("search").innerHTML = "Searching...";
+}
+function kiemtra()
+{
+    let pas1=document.getElementById("password_account").innerHTML,pas2=document.getElementById("rewrite_password_account").innerHTML;
+    if (pas1!=pas2) window.alert("False");
+    else window.alert("True");
+}
+function show_button(buy,detail)
+{
+    document.getElementById(buy).style.display="block";
+    document.getElementById(detail).style.display="block";
+}
+function hide_button(buy,detail)
+{
+    document.getElementById(buy).style.display="none";
+    document.getElementById(detail).style.display="none";
+}
+function show_text()
+{
+    document.getElementById("search").style.display="block";
+}
+function hide_text()
+{
+    document.getElementById("search").style.display="none";
+}
